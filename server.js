b509804c03e4abcca1da58755ca69f179a8836c0
Daisuke Tachihara <tachihara.d@everbank.co.jp>
2022-03-15 22:33:41 +0900

    型定義でクエリを組み立てる

--- server.js
+++ server.js
@@ -1,65 +1,46 @@
 var express = require('express');
 var { graphqlHTTP } = require('express-graphql');
-var { buildSchema } = require('graphql');
+var graphql = require('graphql');
 
-var schema = buildSchema(`
-    input MessageInput {
-        content: String
-        author: String
-    }
-
-    type Message {
-        id: ID!
-        content: String
-        author: String
-    }
-
-    type Query {
-        getMessage(id: ID): Message
-    }
-
-    type Mutation {
-        createMessage(input: MessageInput): Message
-        updateMessage(id: ID!, input: MessageInput): Message
-    }
-`);
-
-class Message {
-    constructor(id, {content, author}) {
-        this.id = id;
-        this.content = content;
-        this.author = author;
-    }
-}
-
-var fakeDatabase = {};
-
-var root = {
-    getMessage: ({id}) => {
-        if (!fakeDatabase[id]) {
-            throw new Error("no message exists with id " + id);
-        }
-
-        return new Message(id, fakeDatabase[id]);
+var fakeDatabase = {
+    'a': {
+        id: 'a',
+        name: 'alice'
     },
-    createMessage: ({input}) => {
-        let id = require('crypto').randomBytes(10).toString('hex');
-        fakeDatabase[id] = input;
-        return new Message(id, input);
-    },
-    updateMessage: ({id, input}) => {
-        if (!fakeDatabase[id]) {
-            throw new Error('no message exists with id ' + id);
-        }
-        fakeDatabase[id] = input;
-        return new Message(id, input);
+    'b': {
+        id: 'b',
+        name: 'bob'
     }
 };
+
+var UserType = new graphql.GraphQLObjectType({
+    name: 'User',
+    fields: {
+        id: {type: graphql.GraphQLString},
+        name: {type: graphql.GraphQLString}
+    }
+});
+
+var queryType = new graphql.GraphQLObjectType({
+    name: 'Query',
+    fields: {
+        user: {
+            type: UserType,
+            args: {
+                id: {type: graphql.GraphQLString}
+            },
+            resolve: (_, {id}) => {
+                return fakeDatabase[id];
+            }
+        }
+    }
+});
+
+var schema = new graphql.GraphQLSchema({query: queryType});
 
 var app = express();
 app.use('/graphql', graphqlHTTP({
     schema: schema,
-    rootValue: root,
     graphiql: true
 }));
 app.listen(4000);
